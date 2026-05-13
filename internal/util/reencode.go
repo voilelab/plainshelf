ade0698e00770900785a4c1ec5b67f1b4d459a50
Mukyu <mudream4869@gmail.com>
2026-05-13 23:45:05 +0800

    Add encoding return value

--- internal/util/reencode.go
+++ internal/util/reencode.go
@@ -7,11 +7,11 @@ import (
 	"golang.org/x/text/encoding/simplifiedchinese"
 )
 
-func ReEncodeToUTF8(src io.ReadSeeker) (io.Reader, error) {
+func ReEncodeToUTF8(src io.ReadSeeker) (io.Reader, string, error) {
 	buf := make([]byte, 1024)
 	n, err := src.Read(buf)
 	if err != nil && err != io.EOF {
-		return nil, Errorf("%w", err)
+		return nil, "", Errorf("%w", err)
 	}
 	buf = buf[:n]
 
@@ -19,15 +19,15 @@ func ReEncodeToUTF8(src io.ReadSeeker) (io.Reader, error) {
 
 	res := chardet.Detect(buf)
 	if res.Confidence < 0.5 {
-		return nil, Errorf("failed to detect encoding with sufficient confidence")
+		return nil, "", Errorf("failed to detect encoding with sufficient confidence")
 	}
 
 	switch res.Encoding {
-	case "UTF-8":
-		return src, nil
+	case "UTF-8", "UTF-8-SIG":
+		return src, res.Encoding, nil
 	case "GB18030", "GBK", "GB2312":
-		return simplifiedchinese.GB18030.NewDecoder().Reader(src), nil
+		return simplifiedchinese.GB18030.NewDecoder().Reader(src), res.Encoding, nil
 	default:
-		return nil, Errorf("unsupported encoding: `%s`", res.Encoding)
+		return nil, "", Errorf("unsupported encoding: `%s`", res.Encoding)
 	}
 }
--- txtlib-srv/handle_import.go
+++ txtlib-srv/handle_import.go
@@ -75,7 +75,7 @@ func (app *App) HandleAPIImportBook(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
-	utf8File, err := util.ReEncodeToUTF8(f)
+	utf8File, _, err := util.ReEncodeToUTF8(f)
 	if err != nil {
 		log.Printf("ReEncodeToUTF8 error: %v", err)
 		http.Error(w, "failed to re-encode uploaded file to UTF-8", http.StatusInternalServerError)
